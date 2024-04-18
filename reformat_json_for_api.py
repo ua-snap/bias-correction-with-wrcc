023e75b2cf8bafbd072127d9ca5604ca709d5785
Charles Parr <cparr4@alaska.edu>
2024-04-18 08:50:51 -0800

    reformat the uncorrected data too

--- reformat_json_for_api.py
+++ reformat_json_for_api.py
@@ -4,17 +4,23 @@ from config import metrics
 
 
 def convert_to_nested_json(input_file, output_file, metric):
-    """Convert a list of dictionaries JSON file to a dictionary of dictionaries JSON file."""
-    # Load the input JSON file
+    """Convert list of dicts JSON file to a dictionary of dictionaries JSON file."""
     with open(input_file, "r") as f:
         data = json.load(f)
-
-    # Convert the list of dictionaries to a dictionary of dictionaries
+    # convert the list of dictionaries to a dictionary of dictionaries
     nested_data = {
         item["SNAP ID"]: item[f"bias corrected {metric} futures"] for item in data
     }
+    with open(output_file, "w") as f:
+        json.dump(nested_data, f, indent=2)
 
-    # Write the nested dictionary to the output JSON file
+
+def convert_uncorrected_to_nested_json(input_file, output_file, metric):
+    """Convert list of dicts JSON file to a dictionary of dictionaries JSON file."""
+    with open(input_file, "r") as f:
+        data = json.load(f)
+    # convert the list of dictionaries to a dictionary of dictionaries
+    nested_data = {item["SNAP ID"]: item[f"biased {metric} futures"] for item in data}
     with open(output_file, "w") as f:
         json.dump(nested_data, f, indent=2)
 
@@ -24,5 +30,10 @@ for metric in metrics:
     convert_to_nested_json(
         f"bias_corrected_{metric}_future_projections.json",
         f"nested_bias_corrected_{metric}_future_projections.json",
+        metric,
+    )
+    convert_uncorrected_to_nested_json(
+        f"uncorrected_{metric}_future_projections.json",
+        f"nested_uncorrected_{metric}_future_projections.json",
         metric,
     )
