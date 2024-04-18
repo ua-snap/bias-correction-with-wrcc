aae0bbf5d1ff0fe27271afdf03d2fd4b266a0480
Charles Parr <cparr4@alaska.edu>
2024-04-18 08:38:45 -0800

    bump wrrc climo thresh and metric names to config

---
+++ config.py
@@ -0,0 +1,7 @@
+metrics = {
+    "heating": "Heating Degree Days Climatology",
+    "freezing_index": "Air Freezing Index Climatology",
+    "thawing_index": "Air Thawing Index Climatology",
+    "below_zero": "Degree Days Below 0F Climatology",
+}
+wrcc_yr_threshold = 20
